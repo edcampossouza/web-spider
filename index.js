15e8fd1dcafd97d9c0519754d513af33bd8679b7
Eduardo Souza <edcampossouza@gmail.com>
2023-05-28 10:39:47 -0300

    spider: callback hell

---
+++ index.js
@@ -0,0 +1,12 @@
+import { spider } from "./spider.js";
+
+spider(process.argv[2], (err, filename, downloaded) => {
+  console.log("here");
+  if (err) {
+    console.error(err);
+  } else if (downloaded) {
+    console.log(`Completed the download of "${filename}"`);
+  } else {
+    console.log(`"${filename}" was already downloaded`);
+  }
+});
